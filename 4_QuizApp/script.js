71156fe1da9496b0a551fc0c77c0fb1eb22eb108
Ankit Raj <109663828+ankitrajcs13@users.noreply.github.com>
2024-01-21 21:38:11 +0530

    Update script.js

--- 4_QuizApp/script.js
+++ 4_QuizApp/script.js
@@ -2,7 +2,7 @@ function validatePin(){
     var pinInp = document.getElementById("pinInput").value;
 
     if(pinInp.length === 6){
-        window.location.href = '/Questions/index.html'
+        window.location.href = 'https://ankitrajcs13.github.io/JavaScript-Projects/4_QuizApp/Questions/index.html'
     }
     else{
         alert("Invalid Pin. Please enter correct pin");
