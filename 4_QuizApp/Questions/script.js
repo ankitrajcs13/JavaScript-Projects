75748f2a235099fd2e41c9c6555366a428db855a
Ankit Raj <109663828+ankitrajcs13@users.noreply.github.com>
2024-02-04 22:40:41 +0530

    Update script.js

--- 4_QuizApp/Questions/script.js
+++ 4_QuizApp/Questions/script.js
@@ -60,9 +60,8 @@ function fetchQuestions(quizKey) {
 
     let heading = document.getElementById("name");
 
-    //var userUid = userCreds.uid; // You need to retrieve the user's UID, either from authentication or another source
-    //const quizRef = ref(db, "User/" + userUid + "/" + quizKey);
-  const quizRef = ref(db, "User/"  + quizKey);
+   var userUid = userCreds.uid; // You need to retrieve the user's UID, either from authentication or another source
+   const quizRef = ref(db, "User/" + userUid + "/" + quizKey);
   
     get(quizRef)
       .then((snapshot) => {
